60bdb3032b2d358607de39b742ca4e542b733b58
Matheus Augusto <matheus.santos7@ufu.br>
2025-03-09 22:14:48 -0300

    Semana 05

---
+++ Semana 05/Exercicio02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        static void RealizarOperacao(int op, string nome, string sobrenome)
+        {
+            Console.WriteLine($"Iniciando operação {op}...");
+            for (int i = 0; i < 1000000000; i++)
+            {
+                var p = new Pessoa(nome, sobrenome, 23);
+                
+            }
+
+            Console.WriteLine($"Fianlizando operação {op}...");
+        }
+
+        static void ExecutarSequencial()
+        {
+            RealizarOperacao(1, "Fulano", "da Silva");
+            RealizarOperacao(2, "Beltrano", "da Silva");
+            RealizarOperacao(3, "Sicrano", "da Silva");
+        }
+
+        static void ExecutarComThreads()
+        {
+            var t1 = new Thread( () =>
+            {
+                RealizarOperacao(1, "Fulano", "da Silva");
+            });
+
+            var t2 = new Thread( () =>
+            {
+                RealizarOperacao(2, "Fulano", "da Silva");
+            });
+
+            var t3 = new Thread( () =>
+            {
+                RealizarOperacao(3, "Fulano", "da Silva");
+            });
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+        }
+
+        static void ExecutarComTasks()
+        {
+            var t1 = Task<int>.Run( () =>
+            {
+                RealizarOperacao(1, "Fulano", "da Silva");
+                return 1;
+            });
+
+            var t2 = Task<int>.Run( () =>
+            {
+                RealizarOperacao(2, "Fulano", "da Silva");
+                return 2;
+            });
+
+            var t3 = Task<int>.Run( () =>
+            {
+                RealizarOperacao(3, "Fulano", "da Silva");
+                return 3;
+            });
+
+            Console.WriteLine($"Tarefa {t1.Result} finalizou.");
+            Console.WriteLine($"Tarefa {t2.Result} finalizou.");
+            Console.WriteLine($"Tarefa {t3.Result} finalizou.");
+        }
+    }
+}
---
+++ Semana 05/Exercicio04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace AsyncAwaitDemo
+{
+    class Program
+    {
+        static async Task Main(string[]args)
+        {
+            string URL ="https://raw.githubusercontent.com/l3oxer/Doggo/main/README.md";
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            var tasks = new List<Task>{ SummonDogLocally(), SummonDogFromURL(URL)};
+            await Task.WhenAll(tasks);
+
+            sw.Stop();
+
+            Console.WriteLine("We are done here ...."+ sw.Elapsed.TotalSeconds);
+
+        }
+
+        static async Task SummonDogLocally()
+        {
+            Console.WriteLine("1. Summoning Dog Locally ...");
+
+            string dogText = await File.ReadAllTextAsync("dog.txt");
+
+            Console.WriteLine($"2. Dog Summoned Locally  \n{dogText}");
+
+        }
+
+        static async Task SummonDogFromURL(string URL)
+        {
+            Console.WriteLine("1.Summoning Dog from URL ...");
+
+            using(var httpClient = new HttpClient())
+            {
+               string result = await httpClient.GetStringAsync(URL);
+               Console.WriteLine($"2.Dog Summoned from URL \n{result}");
+            }
+        }
+    }
+}
