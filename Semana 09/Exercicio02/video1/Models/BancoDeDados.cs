572ab7724c216522051a5622f5fe20970dc569c8
Matheus Augusto <matheus.santos7@ufu.br>
2025-04-05 22:55:55 -0300

    Semana 09

---
+++ Semana 09/Exercicio02/video1/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace video1.Models
+{
+    public class BancoDeDados:DbContext
+    {
+        public DbSet<Cliente> Clients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Crud;Integrated Security=True");
+        }
+    }
+}
---
+++ Semana 09/Exercicio02/video1/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace video1.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Cell { get; set; }
+    }
+}
