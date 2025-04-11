4041661cbbf8333d74d829d454199bc73c266edf
Matheus Augusto <matheus.santos7@ufu.br>
2025-04-11 00:52:18 -0300

    Semana 07

---
+++ Semana 07/Exercicio02/Aula Basica/Aula Basica/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula_Basica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+               
+    }
+}
---
+++ Semana 07/Exercicio02/Aula Basica/Aula Basica/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula_Basica
+{
+    public partial class MainPage : TabbedPage
+    {
+        string nome;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BTNOLA_Clicked(object sender, EventArgs e)
+        {
+            nome = await DisplayPromptAsync("Nome", "Digite seu nome: ", "Ok");
+            await DisplayAlert("Aviso", "Olá " + nome, "Ok");
+        }
+    }
+
+}
